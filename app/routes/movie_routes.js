293ea94f53770cc4a06667f86a8754c454b87870
araceli-ruiz <ruizdotcom@gmail.com>
2020-11-21 23:39:12 -0500

    Finished movies backend

---
+++ app/routes/movie_routes.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const passport = require('passport')
+const Movie = require('../models/movie')
+
+const customErrors = require('../../lib/custom_errors')
+
+const handle404 = customErrors.handle404
+
+const requireOwnership = customErrors.requireOwnership
+
+const removeBlanks = require('../../lib/remove_blank_fields')
+
+const requireToken = passport.authentication('bearer', { session: false })
+
+const router = express.Router()
+
+// INDEX
+router.get('/movies', requireToken, (req, res, next) => {
+  Movie.find()
+    .then(movies => {
+      return movies.map(movie => movie.toObject())
+    })
+    .then(movies => res.status(200).json({ movies: movies }))
+    .catch(next)
+})
+
+// SHOW
+router.get('/movies/:id', requireToken, (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(handle404)
+    .then(movie => res.status(200).json({ movie: movie.toObject() }))
+    .catch(next)
+})
+
+// create
+router.post('/movies', requireToken, (req, res, next) => {
+  req.body.movie.owner = req.user.id
+  Movie.create(req.body.movie)
+    .then(movie => {
+      res.status(201).json({ movie: movie.toObject() })
+    })
+    .catch(next)
+})
+
+// UPDATE
+router.patch('/movies/:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.movie.owner
+
+  Movie.findById(req.params.id)
+    .then(handle404)
+    .then(movie => {
+      requireOwnership(req, movie)
+
+      return movie.updateOne(req.body.movie)
+    })
+})
+
+// DELETE
+router.delete('/movies/:id', requireToken, (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then(handle404)
+    .then(movie => {
+      requireOwnership(req, movie)
+      movie.deleteOne()
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
 const bookRoutes = require('./app/routes/book_routes')
+const movieRoutes = require('./app/routes/movie_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -61,10 +62,11 @@ app.use(express.urlencoded({ extended: true }))
 // log each request as it comes in for debugging
 app.use(requestLogger)
 
-// register route files
+// register route files BOOK ROUTES AND
 app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(bookRoutes)
+app.use(movieRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
